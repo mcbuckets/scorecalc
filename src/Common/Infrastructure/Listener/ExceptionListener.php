9b3ebfc453041d8aa49d7059fedb12f4c14e4ddc
mcbuckets <frbarac@gmail.com>
2021-07-14 16:05:34 +0200

    Basic exception handling

---
+++ src/Common/Infrastructure/Listener/ExceptionListener.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\Infrastructure\Listener;
+
+use Throwable;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Contracts\HttpClient\Exception\HttpExceptionInterface;
+
+final class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $response = $this->createApiResponse($event->getThrowable());
+        $event->setResponse($response);
+    }
+
+    private function createApiResponse(Throwable $exception): JsonResponse
+    {
+        $statusCode = $exception instanceof HttpExceptionInterface ? $exception->getCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        return new JsonResponse(['error_message' => $exception->getMessage(), 'code' => $statusCode], $statusCode);
+    }
+}
